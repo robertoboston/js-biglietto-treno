10fa6c5b24f54f12cb1118a86ab41364162d8298
roberto.boston <roberto.bozzo97@tiscali.it>
2022-11-25 16:38:20 +0100

    add toFixed(2)

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
-let km_tot_user=prompt('Quanti chilometri devi percorrere?')
+let km_tot_user= parseInt(prompt('Quanti chilometri devi percorrere?')) 
 
-let age_user=prompt('indica la tua età')
+let age_user=parseInt (prompt('indica la tua età'))
 
 if(age_user < 18){
     let tot_under_18= km_tot_user * 0.21 * 0.8;
-    console.log(tot_under_18)
+    let n= tot_under_18.toFixed(2)
+    console.log(n)
     
 }
 else if(age_user > 65){
     let tot_under_65= km_tot_user * 0.21 * 0.6;
-    console.log(tot_under_65)
+    let m= tot_under_65.toFixed(2)
+    console.log(m)
     
 }
 else{
     let tot_middle= km_tot_user * 0.21;
-    console.log(tot_middle)
+    let p= tot_middle.toFixed(2)
+    console.log(p)
 
 }
     

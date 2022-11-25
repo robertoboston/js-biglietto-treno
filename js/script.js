a331b51103c049862b909a4e4d5ef9a2b7c52018
roberto.boston <roberto.bozzo97@tiscali.it>
2022-11-25 15:04:28 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let km_tot_user=prompt('Quanti chilometri devi percorrere?')
+
+let age_user=prompt('indica la tua età')
+
+if(age_user < 18){
+    
+}
+else if(age_user > 65){
+    
+
+}
+else{
+    
+
+}
+    

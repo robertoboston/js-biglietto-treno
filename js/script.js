97018f89267653c5919ac254ba12216326177213
roberto.boston <roberto.bozzo97@tiscali.it>
2022-11-27 15:15:25 +0100

    add document.getElementById

--- js/script.js
+++ js/script.js
@@ -8,14 +8,18 @@ if(age_user < 18){
     let tot_under_18= km_tot_user * 0.21 * 0.8;
     let n= tot_under_18.toFixed(2)+euro
     console.log(n)
+    document.getElementById('cost').innerHTML=`${'il prezzo del biglietto è'} ${n}`
 }
 else if(age_user > 65){
     let tot_under_65= km_tot_user * 0.21 * 0.6;
     let m= tot_under_65.toFixed(2)+euro
     console.log(m)
+    document.getElementById('cost').innerHTML=`${'il prezzo del biglietto è'} ${m}`
 }
 else{
     let tot_middle= km_tot_user * 0.21;
     let p= tot_middle.toFixed(2)+euro
     console.log(p)
+    document.getElementById('cost').innerHTML=`${'il prezzo del biglietto è'} ${p}`
 }
+

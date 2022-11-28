e7afeb809d20db91cb19a05bbb1ced3b6a1ce32d
roberto.boston <roberto.bozzo97@tiscali.it>
2022-11-28 10:23:56 +0100

    fixed code

--- js/script.js
+++ js/script.js
@@ -6,14 +6,14 @@ const euro = '€'
 
 if(age_user < 18){
     let tot_under_18= (km_tot_user * 0.21 * 0.8).toFixed(2) + euro;
-    document.getElementById('cost').innerHTML=`${'il prezzo del biglietto scontato under 18 è'} ${tot_under_18}`
+    document.getElementById('cost').innerHTML=`il prezzo del biglietto scontato under 18 è: ${tot_under_18}`
 }
 else if(age_user > 65){
     let tot_under_65= (km_tot_user * 0.21 * 0.6).toFixed(2) + euro;
-    document.getElementById('cost').innerHTML=`${'il prezzo del biglietto scontato under 65 è'} ${tot_under_65}`
+    document.getElementById('cost').innerHTML=`il prezzo del biglietto scontato under 65 è: ${tot_under_65}`
 }
 else{
     let tot_middle= (km_tot_user * 0.21).toFixed(2) + euro;
-    document.getElementById('cost').innerHTML=`${'il prezzo del biglietto è'} ${tot_middle}`
+    document.getElementById('cost').innerHTML=`il prezzo del biglietto è: ${tot_middle}`
 }
 

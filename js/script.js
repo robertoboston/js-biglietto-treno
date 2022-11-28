599a5536f52df9b699975fe52aca9b8f3efccbd1
roberto.boston <roberto.bozzo97@tiscali.it>
2022-11-28 10:17:55 +0100

    ottimize code

--- js/script.js
+++ js/script.js
@@ -1,25 +1,19 @@
-let km_tot_user= parseInt(prompt('Quanti chilometri devi percorrere?')) 
+let km_tot_user=parseInt(prompt('Quanti chilometri devi percorrere?')) 
 
-let age_user=parseInt (prompt('indica la tua età'))
+let age_user=parseInt(prompt('indica la tua età'))
 
 const euro = '€'
 
 if(age_user < 18){
-    let tot_under_18= km_tot_user * 0.21 * 0.8;
-    let n= tot_under_18.toFixed(2)+euro
-    console.log(n)
-    document.getElementById('cost').innerHTML=`${'il prezzo del biglietto è'} ${n}`
+    let tot_under_18= (km_tot_user * 0.21 * 0.8).toFixed(2) + euro;
+    document.getElementById('cost').innerHTML=`${'il prezzo del biglietto scontato under 18 è'} ${tot_under_18}`
 }
 else if(age_user > 65){
-    let tot_under_65= km_tot_user * 0.21 * 0.6;
-    let m= tot_under_65.toFixed(2)+euro
-    console.log(m)
-    document.getElementById('cost').innerHTML=`${'il prezzo del biglietto è'} ${m}`
+    let tot_under_65= (km_tot_user * 0.21 * 0.6).toFixed(2) + euro;
+    document.getElementById('cost').innerHTML=`${'il prezzo del biglietto scontato under 65 è'} ${tot_under_65}`
 }
 else{
-    let tot_middle= km_tot_user * 0.21;
-    let p= tot_middle.toFixed(2)+euro
-    console.log(p)
-    document.getElementById('cost').innerHTML=`${'il prezzo del biglietto è'} ${p}`
+    let tot_middle= (km_tot_user * 0.21).toFixed(2) + euro;
+    document.getElementById('cost').innerHTML=`${'il prezzo del biglietto è'} ${tot_middle}`
 }
 
